142ccb26885535243cb54e04694d5a90143412fe
Joseph123321 <144241862+Joseph123231@users.noreply.github.com>
2023-12-20 14:25:58 +0000

    Testing design for small straight

--- Test.py
+++ Test.py
@@ -1,3 +1,13 @@
 import random
 import math
 
+dice = [1,4,2,3,5]
+dice.sort()
+if len(set(dice)) == 5:
+    if dice[1 + 1] == dice[2] and dice[0]!= dice[4] :
+        print("small straight")
+        print(dice)
+else:
+    print("Not small straight")
+    print(dice)
+
