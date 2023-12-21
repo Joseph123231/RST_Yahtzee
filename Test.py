d497ea88132ece7e27f03d1855dae93a3f6319db
Joseph123321 <144241862+Joseph123231@users.noreply.github.com>
2023-12-21 09:12:52 -0500

    rough large straight added not working propely however

--- Test.py
+++ Test.py
@@ -11,3 +11,8 @@ else:
     print("Not small straight")
     print(dice)
 
+if len(set(dice)) == 5:
+    if dice[0 + 1] == dice[1] and dice[0 + 4] == dice[4]:
+        print("large staright")
+    else:
+        print("not large straight")
