d706a03b864301a0d013de7ba191c26b8992ff03
Joseph123321 <144241862+Joseph123231@users.noreply.github.com>
2023-12-27 23:25:53 -0500

    extended description explained
    In this commit, I added a rough framework for checking the following to the test.py file, 3-of-a-kind, 4-of-a-kind, full house, yahtzee, and small staright. These checking cases will all certainly be changed and improved in the coming days. I removed the previous check for a large straight to find a way that works better

--- Test.py
+++ Test.py
@@ -1,18 +1,46 @@
 import random
 import math
 
-dice = [1,4,2,3,5]
-dice.sort()
-if len(set(dice)) == 5:
-    if dice[1 + 1] == dice[2] and dice[0]!= dice[4] :
-        print("small straight")
-        print(dice)
-else:
-    print("Not small straight")
-    print(dice)
-
-if len(set(dice)) == 5:
-    if dice[0 + 1] == dice[1] and dice[0 + 4] == dice[4]:
-        print("large staright")
+Of_kind = [3,3,2,2,5]
+Of_kind.sort()
+# target = 3
+# score = None
+# _______________________3 of a kind check______________-
+if len(set(Of_kind)) == 2 or len(set(Of_kind)) == 3:
+    if Of_kind[2] == Of_kind[0] or Of_kind[2] == Of_kind[4]:
+        if Of_kind[3] != Of_kind[4] or Of_kind[0] != Of_kind[1]:
+            print("3 of kind")
+            
     else:
-        print("not large straight")
+         print("NOT 3 of a kind")
+         print(Of_kind)
+print(Of_kind)
+#__________________________________4 of a kind check___________________
+if len(set(Of_kind)) == 2:
+    if Of_kind[0] == Of_kind[3] or Of_kind[4] == Of_kind[1]:
+        print("4 of a kind")
+        print(Of_kind)
+    else:
+         print("Not 4 of a kind")
+#________________________________________________________________
+
+#_________________________________ FUll HOUSE CHECK_______________________
+if len(set(Of_kind)) == 2: 
+    if Of_kind[0] != Of_kind[3] or Of_kind[4] != Of_kind[1] and Of_kind[2] == Of_kind[0] or Of_kind[4]:
+        print("full house")
+    else:
+         print("Not Full house")
+#________________________________________________________________________
+#_________________________small straight check__________________ ### need to account for when there are duplicates in the dice but still add u
+if len(set(Of_kind)) == 5 or len(set(Of_kind)) == 4:
+    if Of_kind[1 + 1] == Of_kind[2] and Of_kind[0]!= Of_kind[4] :
+        print("small straight")
+        print(Of_kind)
+    else:
+        print("Not small straight")
+#________________________________________________________________
+#_____________________check fpr YAHTZEE____________
+if len(set(Of_kind)) == 1:
+    print("IT'S A YAHTZEE")
+else:
+    print("IT'S NOT A YAHTZEE")
